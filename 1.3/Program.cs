ac8156191f7e10f22e207cfd08bda463acd2b45f
DESKTOP-LRU92KK\pc <caugioi.lr@gmail.com>
2023-09-30 16:11:20 +0700

    Add project files.

---
+++ 1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Tạo ứng dụng C# Console Applciation minh họa việc khai báo biến, hằng số và sử dụng
+//chúng.
+
+int id = 01;
+string name = "oi zoi oi";
+byte age = 18;
+char gender = 'M';
+const float dtb = 8f;
+
+Console.WriteLine("Student ID: {0}", id);
+Console.WriteLine("Student Name: " + name);
+Console.WriteLine("Student age: " + age);
+Console.WriteLine("Student gender: " +  gender);
+Console.WriteLine("Student DTB: " + dtb);
+
